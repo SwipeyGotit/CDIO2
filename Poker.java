c98f5c919e11828596be80651fcd8485041ee0fc
RoniKhalil17 <s245495@student.dtu.dk>
2024-10-29 20:01:11 +0100

    Add files via upload

---
+++ Poker.java
@@ -0,0 +1,12 @@
+class PokerKonto{
+    private int balance;
+    public int PlayerID;
+    public String PlayerName;
+
+    public PokerKonto(int balance, int PlayerID, String PlayerName){
+        this.balance = balance;
+        this.PlayerID = PlayerID;
+        this.PlayerName = PlayerName;
+    }
+
+}
